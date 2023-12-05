6074500a315f446c005b9d2f1f1a53484b3a5b6d
mississppi <msspstudio@gmail.com>
2023-12-05 20:50:20 +0900

    vpc public,private

--- vpc_public/vpc_public_stack.py
+++ vpc_public/vpc_public_stack.py
@@ -1,19 +1,31 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_ec2 as ec2  
     # aws_sqs as sqs,
 )
 from constructs import Construct
+
+CIDR = '192.168.0.0/16'
 
 class VpcPublicStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "VpcPublicQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        self.vpc = ec2.Vpc(
+            self,
+            'two-tier',
+            cidr=CIDR,
+            max_azs=2,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name='public',
+                    subnet_type=ec2.SubnetType.PUBLIC
+                ),
+                ec2.SubnetConfiguration(
+                    name='private',
+                    subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT
+                ),
+            ]
+        )
